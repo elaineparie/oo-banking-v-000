7cda2d984aaf3f46709aeb73bbd4bd3680dbbdd1
Elaine Anderson <eparie@bu.edu>
2018-06-06 18:27:25 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,7 @@ def valid?
 end
 
 def execute_transaction
-  if !valid?
+  if valid? == false
     puts "Transaction rejected. Please check your account balance."
     @status = "rejected"
   elsif @status != "complete"
